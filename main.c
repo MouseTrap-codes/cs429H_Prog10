02f3105516019db8b151da14804d34329426e1dc
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-03-27 00:10:45 -0500

    a

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include "tdmm.h"
+#include <stdio>
 
 int
 main ()
 {
+  void* ptr1 = tmalloc(100);  
+  void* ptr2 = tmalloc(100);
+  void* ptr3 = tmalloc(100);  
+  void* ptr4 = tmalloc(100);
+  printf("malloced");
+
+  tfree(ptr1);
+  tfree(ptr2);
+  tfree(ptr3);
+  tfree(ptr4);
+  printf("freed");
+
   return 0;
 }
